e8353d621df534ba9f0c41dab7e5f56b42e1f404
Falgun <falgunpatel071@gmail.com>
2026-03-24 01:28:46 +0530

    Register api build and tested successfully

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
-import userModel from "../models/user.model.js";
+import userModel from "./models/user.model.js";
 import crypto from "crypto";
+import jwt from "jsonwebtoken"
+import config from "../config/config.js";
 
 
 export async function register(req,res){
@@ -25,4 +27,25 @@ export async function register(req,res){
         email,
         password: hashedPassword
     })
+
+    const token = jwt.sign({
+        id: user._id
+    }, 
+    config.JWT_SECRET,
+        {
+            expiresIn: '1d'
+        }
+    )
+    res.status(201).json({
+        message: "User registered successfully",
+        user:{
+            username: user.username,
+            email : user.email,
+        },
+        token
+    })
+}
+
+export async function getMe(req,res){
+    
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,12 @@ import * as authController from "../controllers/auth.controllers.js"
 
 const authRouter  = Router();
 
+
+// POST /api/auth/register 
 authRouter.post("/register",authController.register);
+
+// GET /api/auth/get-me
+authRouter.get("/get-me", authController.getMe)
 
 
 export default authRouter;
