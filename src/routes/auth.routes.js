021d21a129e9838c2371e6a23ddbb870174fdc52
Falgun <falgunpatel071@gmail.com>
2026-04-09 01:31:59 +0530

    feat(routes): expose active device endpoints and apply rate limiters to authentication routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,18 @@
 import {Router} from "express";
 import * as authController from "../controllers/auth.controllers.js"
+import { authLimiter } from "../utils/rateLimiter.js";
 
 const authRouter  = Router();
 
 
 // POST /api/auth/register 
-authRouter.post("/register",authController.register);
+authRouter.post("/register", authLimiter, authController.register);
 
 // POST /api/auth/login
-authRouter.post("/login", authController.login)
+authRouter.post("/login", authLimiter, authController.login)
 
 // GET /api/auth/get-me
-authRouter.get("/get-me", authController.getMe)
+authRouter.get("/get-me", authLimiter, authController.getMe)
 
 // GET /api/auth/refresh-token
 authRouter.get("/refresh-token", authController.refreshToken)
@@ -25,5 +26,10 @@ authRouter.get("/logout-all", authController.logoutAll)
 //GET verify email
 authRouter.get("/verify-email", authController.verifyEmail)
 
+// GET /api/auth/sessions -> Returns a list of devices (Chrome, Safari, iPhone, etc)
+authRouter.get("/sessions", authController.getActiveSessions);
+
+// DELETE /api/auth/sessions/:sessionId -> Logs out of a specific device
+authRouter.delete("/sessions/:sessionId", authController.revokeSession);
 
 export default authRouter;
