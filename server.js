ba15967327de1375ed5f414b68efb31fa487ea13
Falgun <falgunpatel071@gmail.com>
2026-04-09 01:31:59 +0530

    feat(server): initialize background background cron jobs and global rate limiter

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
+import { startCleanupJobs } from "./src/jobs/cleanup.js";
+import { globalLimiter } from "./src/utils/rateLimiter.js";
 
 connectDB()
-
+startCleanupJobs()
+app.use(globalLimiter)
 app.get("/", (req, res) => {
   res.send("Authentication API is running 🚀");
 });
